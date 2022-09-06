50a96016d9b64854d6a60dc53af88abf5af98563
yotamos5699 <m.lhafakot@gmail.com>
2022-09-06 22:27:07 +0300

    -cors move

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// **************** DB SERVER ********* 
+// **************** DB SERVER *********
 
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
@@ -9,22 +9,18 @@ const FBrouter = require("./routs/fireBaseRouts");
 const MGrouter = require("./routs/mongoDbRouts");
 const cors = require(`cors`);
 app.use(express.json());
-
-
-app.use(FBrouter);
-app.use(MGrouter);
 app.use(
   cors({
     origin: "*",
   })
 );
 
+app.use(FBrouter);
+app.use(MGrouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
 
 app.listen(PORT, (err) =>
   console.log(`server ${err ? " on" : "listening"} port` + PORT)
